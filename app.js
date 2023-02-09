3d25be8086c2086e4b2d85904de4691391b915c6
RokoPlays <93613532+RokoPlays@users.noreply.github.com>
2023-02-09 12:51:25 +0100

    WhiteRP

---
+++ app.js
@@ -0,0 +1,15 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        } else {
+            entry.target.classList.remove("show");
+        }
+    })
+})
+
+const hidden = document.querySelectorAll(".hidden");
+
+hidden.forEach((entry) => {
+    observer.observe(entry);
+})
